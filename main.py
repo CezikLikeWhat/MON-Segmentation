049bf70ff515c91fe3d1afef30b542faba7393bf
CezikLikeWhat <cezarymackowski99@gmail.com>
2024-01-24 00:24:22 +0100

    main.py refactor

--- main.py
+++ main.py
@@ -3,15 +3,8 @@ import sys
 from PyQt5 import Qt
 
 from View.MainWindow import MainWindow
-from backups.backup_MainWindow import MainWindow as BackupMainWindow
 
 if __name__ == "__main__":
     app = Qt.QApplication(sys.argv)
-    debug = True
-
-    if debug:
-        window = MainWindow()
-    else:
-        window = BackupMainWindow()
-
+    window = MainWindow()
     sys.exit(app.exec_())
